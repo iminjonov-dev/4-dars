04d0fb9e82f5db48ef628c2a3440f8e6c457b4b2
iminjonov-dev <161212692+iminjonov-dev@users.noreply.github.com>
2024-07-30 13:19:02 +0500

    views.py

--- online_shop/views.py
+++ online_shop/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 
-from online_shop.models import Product
+from online_shop.models import Product, Category
 
 
 def products_list(request):
-    return render(request, 'online_shop/home.html')
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'online_shop/home.html', context)
 
 def products_all_view(request):
-    products = Product.objects.all()
-    return render(request, 'online_shop/detail.html')
+    products = Product.objects.get(pk=id)
+    context = {'products': products}
+    return render(request, 'online_shop/detail.html', context)
 
+def category(request):
+    categories = Category.objects.all()
+    context = {'categories': categories}
+    return render(request, 'online_shop/home.html', context)
